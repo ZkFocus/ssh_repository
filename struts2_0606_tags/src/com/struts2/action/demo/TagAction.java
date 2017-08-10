67ca148c41c98f8cf064722c3b8fcc9fd10dec23
Zhou Kun <503096965@qq.com>
2017-08-10 16:52:39 +0800

    struts2 tags demo
    Signed-off-by: Zhou Kun <503096965@qq.com>

---
+++ struts2_0606_tags/src/com/struts2/action/demo/TagAction.java
@@ -0,0 +1,19 @@
+package com.struts2.action.demo;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TagAction extends ActionSupport{
+
+	private Person person;
+	public String execute(){
+		this.addFieldError("fielderror", "name is error");
+		return SUCCESS;
+	}
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+}
